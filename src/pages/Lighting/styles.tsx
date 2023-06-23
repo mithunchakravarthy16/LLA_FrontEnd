20378f64d0b944806e43d6e9e8731d54bcca2973
Mithun Chakravarthy <mithunch@infinite.com>
2023-06-23 15:22:03 +0530

    lighting page implementations

--- src/pages/Lighting/styles.tsx
+++ src/pages/Lighting/styles.tsx
@@ -8,7 +8,7 @@ const useStyles = makeStyles({
     paddingLeft: "3.4px",
   }),
   mainSection: (props: any) => ({
-    height: "calc(100vh - 50px)",
+    height: "calc(100vh - 60px)",
     color: "white",
     background: "#161515",
     opacity: 1,
@@ -79,5 +79,92 @@ const useStyles = makeStyles({
   notificationPanelGrid: (props: any) => ({
     border : "1px solid #333333"
   }),
+
+
+  liveContainer: (props: any) => ({
+    display: "flex",
+    height: "100%",
+    flexDirection: "column",
+    justifyContent: "space-around",
+    backgroundColor: "#57585A20",
+    borderRadius: "4px",
+    position: "relative",
+  }),
+
+  liveImgStyle: (props: any) => ({
+    position: "absolute",
+    top: "-15px",
+    left: "20px",
+  }),
+
+  liveContentValue: (props: any) => ({
+    fontSize: "20px",
+    fontWeight: 600,
+  }),
+
+  liveContentValueGreen: (props: any) => ({
+    fontSize: "22px",
+    fontWeight: 700,
+    color: props?.palette?.gridViewComponentCommonStyle?.liveContentValueGreen, //"#80C53B",
+  }),
+
+  liveContentLabel: (props: any) => ({
+    fontSize: "14px",
+    fontWeight: 500,
+    fontStyle: "italic",
+  }),
+
+  liveContentLabelGreen: (props: any) => ({
+    fontSize: "14px",
+    fontWeight: 500,
+    fontStyle: "italic",
+    display: "flex",
+    alignItems: "center",
+    columnGap: "15px",
+  }),
+
+  liveContentStyle: (props: any) => ({
+    flex: 1,
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "center",
+    alignItems: "center",
+    rowGap: "10px",
+  }),
+
+  liveContentLeftStyle: (props: any) => ({
+    flex: 1,
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "center",
+    alignItems: "center",
+    rowGap: "10px",
+    position: "relative",
+    "&::after": {
+      content: '""',
+      position: "absolute",
+      left: "28%",
+      bottom: 0,
+      width: "44%", // Specify the desired length of the bottom border
+      borderBottom: `1px solid ${props?.palette?.gridViewComponentCommonStyle?.liveContentLeftBorder}`, // Specify your desired color and border style
+      opacity: "0.4",
+    },
+  }),
+
+  aqiCircleStyle: (props: any) => ({
+    
+    width: "6px",
+    height: "6px",
+    borderRadius: "50%",
+    backgroundColor: props?.palette?.gridViewComponentCommonStyle?.aqiCircleBg, //"#80C43B",
+    boxShadow: `0px 0px 2px 3px ${props?.palette?.gridViewComponentCommonStyle?.aqiCircleShadow}`,
+    
+  }),
+
+  graphTwoHeader: (props:any)=>({
+    height: "10%",
+    
+  }),
+
 });
 export default useStyles;
