bc30e64ba09001da9861e9c3dd5c68042e26013f
Tulasi Narayana Dattu Suggala <tulasi.narayana@infinite.com>
2023-06-23 11:35:00 +0530

    updated icons

--- src/assets/topPanelListIcons/index.js
+++ src/assets/topPanelListIcons/index.js
@@ -2,5 +2,20 @@ import CoTwoCloudIcon from "./co2CloudIcon.svg";
 import VocCloudIcon from "./vocCloudIcon.svg";
 import AirQualityIcon from "./airQualityIcon.svg";
 import PersonIcon from "./personIcon.svg";
+import PowerConsumtionIcon from "./powerConsumption.svg";
+import TemperatureIcon from "./temperature.svg";
+import WaterConsumption from "./waterConsumption.svg";
+import IncomeIcon from "./Income.svg";
+import SubtractIcon from "./Subtract.svg";
 
-export { CoTwoCloudIcon, VocCloudIcon, AirQualityIcon, PersonIcon };
+export {
+  CoTwoCloudIcon,
+  VocCloudIcon,
+  AirQualityIcon,
+  PersonIcon,
+  PowerConsumtionIcon,
+  TemperatureIcon,
+  WaterConsumption,
+  IncomeIcon,
+  SubtractIcon,
+};
