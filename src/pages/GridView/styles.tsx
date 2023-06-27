8a5f2c060054e307bfe51a3c8039ccfa742e4ede
Mithun Chakravarthy <mithunch@infinite.com>
2023-06-27 19:54:48 +0530

    grid page height adjustment for 4k

--- src/pages/GridView/styles.tsx
+++ src/pages/GridView/styles.tsx
@@ -10,24 +10,31 @@ const useStyles = makeStyles({
     height: "100vh",
   }),
   mainSection: (props: any) => ({
-    height: "calc(100vh - 60px)",
+
+    height: "calc(100vh - 50px)",
     color: "white",
     background: "#161515",
     opacity: 1,
     [muiTheme.breakpoints.up(3839)]: {
       width: "calc(100vw - 200px) !important",
+      height: "calc(100vh - 124px)",
     },
     [muiTheme.breakpoints.down(3073)]: {
       width: "calc(100vw - 200px) !important",
+      height: "calc(100vh - 96px)",
+      
     },
     [muiTheme.breakpoints.down(2049)]: {
       width: "calc(100vw - 130px) !important",
+      height: "calc(100vh - 60px)",
     },
     [muiTheme.breakpoints.down(1921)]: {
       width: "calc(100vw - 100px) !important",
+      height: "calc(100vh - 60px)",
     },
     [muiTheme.breakpoints.down(1153)]: {
       width: "calc(100vw - 80px) !important",
+      height: "calc(100vh - 48px)",
     },
 
     "& > .MuiGrid-item:first-child": {
