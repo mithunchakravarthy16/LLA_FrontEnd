482deb9f8742f9dbe0b8a6a4547fd1123f935184
Ezhilarasi Durai <ezhilarasid@infinite.com>
2023-06-28 20:48:39 +0530

    asset tracking api format added

---
+++ src/mockdata/assetTrackingAPI.ts
@@ -0,0 +1,70 @@
+const assetTrackingData = {
+    infoData: {
+      assetTracked : 52,
+      location : 30,
+      outOfGeofence : 10,
+      securityIncident : 75
+    },
+    analytics: {},
+    notifications: {
+        "events": {
+          "totalCount": 0,
+          "eventsList": [
+            {
+              "notificationId": "string",
+              "notificationType": "string",
+              "reason": "string",
+              "tripId": 0,
+              "vehicleId": 0,
+              "driverId": 0,
+              "vehicleName": "string",
+              "driverName": "string",
+              "notificationDate": "2023-06-28T09:22:50.124Z",
+              "lattitude": 0,
+              "longitude": 0,
+              "location": "string"
+            }
+          ]
+        },
+        "incidents": {
+          "totalCount": 0,
+          "incidentList": [
+            {
+              "notificationId": "string",
+              "notificationType": "string",
+              "reason": "string",
+              "tripId": 0,
+              "vehicleId": 0,
+              "driverId": 0,
+              "vehicleName": "string",
+              "driverName": "string",
+              "notificationDate": "2023-06-28T09:22:50.124Z",
+              "lattitude": 0,
+              "longitude": 0,
+              "location": "string"
+            }
+          ]
+        },
+        "alerts": {
+          "totalCount": 0,
+          "alertList": [
+            {
+              "notificationId": "string",
+              "notificationType": "string",
+              "reason": "string",
+              "tripId": 0,
+              "vehicleId": 0,
+              "driverId": 0,
+              "vehicleName": "string",
+              "driverName": "string",
+              "notificationDate": "2023-06-28T09:22:50.124Z",
+              "lattitude": 0,
+              "longitude": 0,
+              "location": "string"
+            }
+          ]
+        }
+      }
+  };
+  
+  export default assetTrackingData;
