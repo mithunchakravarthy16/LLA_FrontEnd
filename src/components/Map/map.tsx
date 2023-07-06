f8bf2d1b6d19185b9bf69a9a1d409519b8c03201
Mithun Chakravarthy <mithunch@infinite.com>
2023-07-06 22:45:34 +0530

    map and sidebar logo implementation

--- src/components/Map/map.tsx
+++ src/components/Map/map.tsx
@@ -15,6 +15,7 @@ import {
 } from "@react-google-maps/api";
 import MapMarker from "components/Marker";
 import customMapStyles from "./customMapStyles";
+import customLightThemeMapStyles from "./customLightThemeMapStyles";
 import theme from "../../theme/theme";
 import appData from "../../data/appData";
 import NotificationListItems from "components/NotificationListItems";
@@ -481,7 +482,7 @@ const Map: React.FC<any> = (props) => {
 
   const getMapTypeControls = () => {
     const defaultMapOptions = {
-      styles: selectedTheme === "light" ? [] : customMapStyles,
+      styles: selectedTheme === "light" ? customLightThemeMapStyles : customMapStyles,
       
     };
     return {
