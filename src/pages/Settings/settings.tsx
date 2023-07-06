6d625ae88555c6856cc21a252e946243dd38f8d6
Kumar Alla <kumara@infinite.com>
2023-07-06 13:56:50 +0530

    Theming changes

--- src/pages/Settings/settings.tsx
+++ src/pages/Settings/settings.tsx
@@ -1,21 +1,75 @@
 import { useState, useEffect } from "react";
-import Grid from "@mui/material/Grid";
+import { useDispatch } from "react-redux";
+import { setLanguage } from "../../redux/actions/loginActions";
 import theme from "../../theme/theme";
+import useTranslation from "../../localization/translations";
+import { useLanguageContext } from "../../localization/languageContext";
 import useStyles from "./styles";
 
 const Settings = () => {
-  const [selectedTheme, setSelectedTheme] = useState(
-    JSON.parse(localStorage.getItem("theme")!)
-  );
+  const { changeTheme, changeLang, profile } = useTranslation();
+  const { language, changeLanguage } = useLanguageContext();
+
+  const [selectedTheme, setSelectedTheme] = useState(JSON.parse(localStorage.getItem('theme')!));
   const [appTheme, setAppTheme] = useState(theme?.defaultTheme);
-  const {} = useStyles(appTheme);
+
+  useEffect(() => {
+    switch (selectedTheme) {     
+      case "light":
+        setAppTheme(theme?.lightTheme);
+        break;
+      case "dark":
+        setAppTheme(theme?.darkTheme);
+        break;
+      default:
+        setAppTheme(theme?.defaultTheme);
+        break;
+    }
+  }, [selectedTheme]);
+
+
+  const handleTheme = (selectedTheme: any) => {
+    switch (selectedTheme) {
+      case "light":
+        setAppTheme(theme?.lightTheme);
+        break;
+      case "dark":
+        setAppTheme(theme?.darkTheme);
+        break;
+      default:
+        setAppTheme(theme?.defaultTheme);
+        break;
+    }
+    localStorage.setItem("theme", JSON.stringify(selectedTheme));
+    setSelectedTheme(selectedTheme);
+    window.location.reload();
+  };
+
+  const dispatch = useDispatch();
+
+  const handleLanguage = (lang:any) =>{
+    changeLanguage(lang);
+    dispatch(setLanguage({lang}));
+  }
 
   return (
     <>
-      <Grid container>
-        <Grid item>Settings</Grid>
-      </Grid>
-    </>
+    <div >Settings</div><br />
+
+    <div>Change Theme</div>
+    <select value={selectedTheme} onChange={(e) => handleTheme(e.target.value)}>
+      <option value="light">Light theme</option>       
+      <option value="dark">Dark theme</option>
+      <option value="default">Default theme</option>
+    </select> <br /><br />
+
+    <div>Change Language</div>
+    <select value={language} onChange={(e) => handleLanguage(e.target.value)}>
+      <option value="en">English</option>
+      <option value="it">Italian</option>
+      <option value="sp">Spanish</option>
+    </select>
+  </>
   );
 };
 
