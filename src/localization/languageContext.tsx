1c51ddf58e330398c24bfe068000f4f2712526dd
Mithun Chakravarthy <mithunch@infinite.com>
2023-08-22 15:05:05 +0530

    implementation of localization 1

--- src/localization/languageContext.tsx
+++ src/localization/languageContext.tsx
@@ -14,7 +14,7 @@ type languagaProps = React.PropsWithChildren<{}>;
 
 export default function LanguageContextProvider({ children}: languagaProps) { 
   const lang = useSelector((state:any) => state?.login?.languageData?.lang);
-  const [language, changeLanguage] = useState(lang ?lang : 'en');
+  const [language, changeLanguage] = useState(lang ? lang : 'en');
   localStorage.setItem("language", JSON.stringify(language));
   return (
     <LanguageContext.Provider value={{ language, changeLanguage }}>
